1f0b39f8aca163f8043f2661d8ce32484f4e1400
Suvish Thoovamalayil <vishy1618@gmail.com>
2024-10-17 07:44:54 +0530

    Remove generateStaticParams so that the art pages are not built. Also, force-dynamic the pages

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,13 +1,14 @@
-import { Metadata } from "next";
-import { notFound } from "next/navigation";
-import { getAllPosts, getPostBySlug } from "@/lib/api";
-import { CMS_NAME } from "@/lib/constants";
-import markdownToHtml from "@/lib/markdownToHtml";
-import Alert from "@/app/_components/alert";
-import Container from "@/app/_components/container";
-import Header from "@/app/_components/header";
-import { PostBody } from "@/app/_components/post-body";
-import { PostHeader } from "@/app/_components/post-header";
+import { Metadata } from 'next';
+import { notFound } from 'next/navigation';
+
+import Container from '@/app/_components/container';
+import Header from '@/app/_components/header';
+import { PostBody } from '@/app/_components/post-body';
+import { PostHeader } from '@/app/_components/post-header';
+import { getPostBySlug } from '@/lib/api';
+import markdownToHtml from '@/lib/markdownToHtml';
+
+export const dynamic = 'force-dynamic';
 
 export default async function Post({ params }: Params) {
   const post = getPostBySlug(params.slug);
@@ -59,12 +60,4 @@ export function generateMetadata({ params }: Params): Metadata {
       images: [post.coverImage],
     },
   };
-}
-
-export async function generateStaticParams() {
-  const posts = getAllPosts();
-
-  return posts.map((post) => ({
-    slug: post.description,
-  }));
 }
